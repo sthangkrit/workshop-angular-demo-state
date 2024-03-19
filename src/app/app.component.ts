0d306770db90616d8662f05165dace675dc5d3f7
Tanapon Krittinaddakul (KSN) <Tanapon.Krittinaddakul@krungsrinimble.com>
2024-03-19 14:19:37 +0700

    child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <div>
+      <a [routerLink]="['/parent']">Parent</a> | <a [routerLink]="['/child']">Child</a>
+    </div>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
-  title = 'demo-state';
 }
