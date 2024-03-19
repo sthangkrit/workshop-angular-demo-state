44c800dd92532d3b7661d6c7ac8ff0cd66e30566
Tanapon Krittinaddakul (KSN) <Tanapon.Krittinaddakul@krungsrinimble.com>
2024-03-19 14:34:11 +0700

    update child

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  private messageSubject = new BehaviorSubject<string[]>([]);
+  messages$ = this.messageSubject.asObservable();
 
-  constructor() { }
+  addNewMessage(newMessage: string) {
+    console.log("newMessage : " + newMessage);
+    
+    this.messageSubject.next([...this.messageSubject.value, newMessage]);
+  }
 }
